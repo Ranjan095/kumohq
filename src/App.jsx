108ff89d53a0b421c245a2890d5e0051d4dbcf54
Ranjan Yadav <113471389+Ranjan095@users.noreply.github.com>
2024-05-31 22:09:45 +0530

    animation update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Dashboard from "./components/Dashboard";
 import Dashbord2 from "./components/Dashbord2";
 import Dashbord3 from "./components/Dashbord3";
 import Dashboard4 from "./components/Dashboard4";
-import Dashboard6 from "./components/Dashbord6";
+import Dashbord6 from "./components/Dashbord6";
 import Dashboard8 from "./components/Dashboard8";
 import Dashboard9 from "./components/Dashboard9";
 import Footer from "./components/Footer";
@@ -20,7 +20,7 @@ function App() {
       <Dashbord2 />
       <Dashbord3 />
       <Dashboard4 />
-      <Dashboard6 />
+      <Dashbord6 />
       <Dashboard8 />
       <Dashboard9 />
       <Footer />
